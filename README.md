d05eb22902db3fc0f581e71648ea81c150dbbcde
Ginjiro Ueta <g.ueta@mmj.ne.jp>
2020-06-01 07:38:33 +0900

    fish functionを修正

---
+++ README.md
@@ -0,0 +1,9 @@
+# mutagen-check
+https://docs.docker.com/docker-for-mac/mutagen-caching/
+Dockerに新しく追加されたMutagen-based cachingでどれだけhostとcontainer間でのファイルのやり取りが早くなったか測定できる
+
+## How to
+> source time-compose
+> ls | grep yml | xargs -I% -L 1 fish -c "time-compose %"
+標準出力を確認するか、result_docker-compose.foo.yml.txtから10回の試行結果を確認できる
+
--- time-compose.fish
+++ time-compose.fish
@@ -1,9 +1,10 @@
 function time-compose
     for i in (seq 10)
         set start (python -c "import time; print(str(int(round(time.time() * 1000))))")
-        time docker-compose -f $argv[1] up --build > /dev/null
+        docker-compose -f $argv[1] up --build > /dev/null
         set end (python -c "import time; print(str(int(round(time.time() * 1000))))") 
         set duration (math $end - $start)
         echo $duration >> result_$argv[1].txt
+        echo $i Executed in "$duration"milliseconds
     end
 end
